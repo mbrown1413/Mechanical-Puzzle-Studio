330434963509fdc89509ed2cc0edef3db2452ea4
Michael Brown <michael@msbrown.net>
2023-10-22 08:00:30 -0400

    Viewpoint/layer drawing in DisplayWebGl

--- src/components/DisplayWebGl.vue
+++ src/components/DisplayWebGl.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed, watch } from 'vue'
 import * as THREE from 'three'
-import { Vector2, Vector3 } from 'three'
+import { Vector3 } from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { ConvexGeometry } from 'three/addons/geometries/ConvexGeometry.js'
 
+import { Coordinate } from "../types.ts"
 import { Grid } from "../grid.ts"
 import { Piece } from  "../puzzle.ts"
+import VerticalSlider from "./VerticalSlider.vue"
 
 const props = defineProps<{
     grid: Grid,
@@ -14,10 +16,17 @@ const props = defineProps<{
 }>()
 
 const el = ref()
+const viewpoints = props.grid.getViewpoints()
+const viewpointId = ref(viewpoints[0].id)
+const layerN = ref(0)
+
+const viewpoint = computed(() =>
+  viewpoints.find(v => v.id === viewpointId.value)
+)
 
 const renderer = new THREE.WebGLRenderer({antialias: true})
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 10);
 camera.position.set(-2, -2, 2)
 //const camera = new THREE.OrthographicCamera(-5.5, 5.5, -5.5, 5.5);
 
@@ -25,21 +34,92 @@ let controls = new OrbitControls(camera, renderer.domElement)
 controls.listenToKeyEvents(window)
 controls.addEventListener('change', refresh)
 
-const light = new THREE.DirectionalLight(0xffffff, 3)
-light.position.set(-5, -5, -5)
-light.lookAt(new Vector3(0, 0, 0))
-scene.add(light)
+function buildObjects() {
+    const objs = []
+    for(let coordinate of props.grid.getCoordinates()) {
+        const cellInfo = props.grid.getCellInfo(coordinate)
+        for(let polygon of Object.values(cellInfo.sidePolygons)) {
+            const inLayer = layerHasCoordinate(coordinate)
+            const hasPiece = coordinateHasPiece(coordinate)
+            let material, geometry, obj
 
-const material = new THREE.MeshPhongMaterial({color: 0xffffff, emissive: 0x444444, side: THREE.DoubleSide});
+            if(hasPiece) {
+                material = new THREE.MeshPhongMaterial({
+                    color: inLayer ? 0x00ff00 : 0xffffff,
+                    emissive: 0x004400,
+                    side: THREE.DoubleSide,
+                    transparent: !inLayer,
+                    opacity: inLayer ? 1 : 0.1,
+                    depthWrite: inLayer,
+                })
+                geometry = new ConvexGeometry(polygon)
+                obj = new THREE.Mesh(geometry, material)
 
-for(let coordinate of props.piece.coordinates) {
-    const cellInfo = props.grid.getCellInfo(coordinate)
-    for(let polygon of Object.values(cellInfo.sidePolygons)) {
-        const geometry = new ConvexGeometry(polygon)
-        const mesh = new THREE.Mesh(geometry, material)
-        scene.add(mesh)
+            } else {
+
+                material = new THREE.LineBasicMaterial({
+                    color: inLayer ? 0x00ff00 : 0xffffff,
+                    transparent: !inLayer,
+                    opacity: inLayer ? 1 : 0.1,
+                })
+                geometry = new THREE.BufferGeometry().setFromPoints(polygon)
+                obj = new THREE.LineLoop(geometry, material)
+                
+                // Draw wireframes of the current layer last so it's not
+                // overwritten by other sides of non-selected layers.
+                if(inLayer) {
+                    obj.renderOrder = 1
+                }
+
+            }
+            objs.push(obj)
+        }
     }
+    return objs
 }
+
+let firstSceneBuild = true
+function rebuildScene() {
+    scene.clear()
+
+    const light1 = new THREE.DirectionalLight(0xffffff, 3)
+    light1.position.set(5, 5, 5)
+    light1.lookAt(new Vector3(0, 0, 0))
+    scene.add(light1)
+
+    const light2 = new THREE.DirectionalLight(0xffffff, 3)
+    light2.position.set(-5, -5, -5)
+    light2.lookAt(new Vector3(0, 0, 0))
+    scene.add(light2)
+    
+    for(let obj of buildObjects()) {
+        scene.add(obj)
+    }
+    
+    // Center view on the center of the grid
+    // Only do this once on initialization though, since the user can
+    // right-click drag to change the OrbitControls target location.
+    if(firstSceneBuild) {
+        const boundingBox = new THREE.Box3().setFromObject(scene)
+        const center = boundingBox.getCenter(new Vector3())
+        camera.lookAt(center)
+        controls.target = center
+    }
+
+    // Add axes helper after bounding box is computed so it doesn't affect the
+    // center.
+    const axesHelper = new THREE.AxesHelper()
+    axesHelper.position.set(-1, -1, -1)
+    scene.add(axesHelper)
+
+    
+    firstSceneBuild = false
+}
+
+watch([viewpoint, layerN], () => {
+    rebuildScene()
+    refresh()
+})
 
 function refresh() {
     if(el.value === null) return
@@ -56,18 +136,60 @@ onMounted(() => {
     resizeObserver.observe(el.value)
 
     el.value.appendChild(renderer.domElement);
+    rebuildScene()
     refresh();
 })
+
+function layerHasCoordinate(coordinate: Coordinate) {
+    let layerCoordinates = props.grid.getViewpointLayer(
+        viewpointId.value,
+        Number(layerN.value)
+    )
+
+    let cordToStr = (cord: Coordinate) => cord.join(",")
+    let coordSet: Set<string> = new Set(layerCoordinates.map(cordToStr))
+    return coordSet.has(cordToStr(coordinate))
+}
+
+function coordinateHasPiece(coordinate: Coordinate) {
+    let cordToStr = (cord: Coordinate) => cord.join(",")
+    let coordSet: Set<string> = new Set(props.piece.coordinates.map(cordToStr))
+    return coordSet.has(cordToStr(coordinate))
+}
 
 </script>
 
 <template>
-    <div class="display2d" ref="el"></div>
+    <div class="display2d" ref="el">
+        <div class="controls">
+            <select v-model="viewpointId">
+                <option
+                    v-for="viewpoint in viewpoints"
+                    :value="viewpoint.id"
+                >
+                    {{ viewpoint.name }}
+                </option>
+            </select>
+            <VerticalSlider
+                v-if="viewpoint !== undefined && viewpoint.nLayers > 1"
+                v-model="layerN"
+                :options="[...Array(viewpoint.nLayers).keys()]"
+            />
+        </div>
+    </div>
 </template>
 
 <style scoped>
 .display2d {
     width: 100%;
     height: 100%;
+    position: relative;  /* Make this the containing block for .controls */
+}
+.controls {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: rgba(255, 255, 255, 0.5);
+    border-bottom-right-radius: 10px;
 }
 </style>
