034a24eee3591eb33ce512fbd3dddcc2a18621c5
Michael Brown <michael@msbrown.net>
2024-03-15 13:49:30 -0400

    Status in solutions list
    This prevents confusion with a blank solutions list by indicating
    whether there are zero solutions, or the solver just hasn't run. It also
    gives a status if the solver is currently running, or has run but
    errored.
    
    Fixes: #20

--- ui/components/SolutionList.vue
+++ ui/components/SolutionList.vue
@@ -3,6 +3,9 @@ import {computed, ref, Ref} from "vue"
 
 import {Puzzle} from "~lib"
 import ListSelect from "~/ui/common/ListSelect.vue"
+import {taskRunner} from "~/ui/globals.ts"
+import {TaskInfo} from "~/ui/TaskRunner.ts"
+import {ProblemSolveTask} from "~/ui/tasks.ts"
 
 const props = defineProps<{
     puzzle: Puzzle,
@@ -43,6 +46,19 @@ const selectedSolutions = computed(() => {
     return solutions
 })
 
+function taskInfoMatchesProblem(taskInfo: TaskInfo): boolean {
+    return taskInfo.task instanceof ProblemSolveTask && taskInfo.task.problemId === props.problemId
+}
+
+const taskInfo = computed(() => {
+    for(const taskInfo of [taskRunner.current, ...taskRunner.queue, ...taskRunner.finished]) {
+        if(taskInfo && taskInfoMatchesProblem(taskInfo)) {
+            return taskInfo
+        }
+    }
+    return null
+})
+
 defineExpose({
     selectedSolutions
 })
@@ -51,11 +67,44 @@ defineExpose({
 <template>
     <div class="solution-list">
         <h4>Solutions</h4>
+
         <ListSelect
+            v-if="solutionItems.length"
             :items="solutionItems"
             :selectedIds="selectedSolutionIds.map(String)"
             @update:selectedIds="selectedSolutionIds = $event.map(Number)"
         />
+
+        <div v-else class="no-solutions-message">
+            <template v-if="problem?.solutions?.length === 0">
+                (zero solutions)
+            </template>
+
+            <template v-else-if="taskInfo?.error">
+                Solver failed<br>
+                {{ taskInfo.error }}
+            </template>
+
+            <template v-else-if="taskInfo">
+                Task {{ taskInfo.status }}
+                <template v-if="taskInfo.status === 'running'">
+                    <VProgressCircular
+                        :indeterminate="taskInfo.progressPercent === null"
+                        :model-value="(taskInfo.progressPercent || 0) * 100"
+                        color="blue-darken-1"
+                        :size="25"
+                        class="progress"
+                    />
+                    <template v-if="taskInfo.progressPercent !== null">
+                        {{ (taskInfo.progressPercent*100).toFixed(2) }}%
+                    </template>
+                </template>
+            </template>
+
+            <template v-else>
+                Run the solver on the Problems tab
+            </template>
+        </div>
     </div>
 </template>
 
@@ -70,5 +119,8 @@ defineExpose({
 }
 .solution-list .list-container {
     flex-grow: 1;
+}
+.solution-list .no-solutions-message {
+    padding: 1em;
 }
 </style>
