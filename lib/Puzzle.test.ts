6e8343eec83e8d36201a6d3ffc139cfd64955980
Michael Brown <michael@msbrown.net>
2024-03-28 01:30:06 -0400

    Voxel attributes

--- lib/Puzzle.test.ts
+++ lib/Puzzle.test.ts
@@ -3,9 +3,9 @@ import {test, expect, describe} from "vitest"
 import {Piece, Puzzle} from "~/lib/Puzzle.ts"
 import {CubicGrid} from "~/lib/grids/CubicGrid.ts"
 
-describe("Puzzle", () => {
-    const grid = new CubicGrid()
+const grid = new CubicGrid()
 
+describe("Piece", () => {
     test("Piece editing", () => {
         const puzzle = new Puzzle(grid)
 
@@ -45,8 +45,10 @@ describe("Puzzle", () => {
         puzzle.removePiece("piece-0")
         expect(puzzle.hasPiece("piece-0")).toBeFalsy()
     })
+})
 
-    test("Piece copy", () => {
+describe("Piece", () => {
+    test("copy", () => {
         const piece0 = new Piece("piece-0", [])
         const copy0 = piece0.copy()
         expect(copy0).not.toBe(piece0)
@@ -56,5 +58,83 @@ describe("Puzzle", () => {
         const copy1 = piece1.copy()
         expect(copy1).not.toBe(piece1)
         expect(copy1).toEqual(piece1)
+    })
+
+    test("equality", () => {
+        const piece1 = new Piece(null, grid.getDefaultPieceBounds())
+        const piece2 = new Piece("withId", grid.getDefaultPieceBounds())
+
+        piece1.voxels = ["0,0,0", "1,1,1"]
+        piece2.voxels = ["1,1,1", "0,0,0"]
+        expect(piece1.equals(piece2)).toBeTruthy()
+
+        piece1.voxels.push("0,0,0")
+        expect(piece1.equals(piece2)).toBeTruthy()
+
+        piece1.voxels.push("0,1,0")
+        expect(piece1.equals(piece2)).toBeFalsy()
+    })
+
+    test("equality with voxelAttribute", () => {
+        const piece1 = new Piece(null, grid.getDefaultPieceBounds())
+        const piece2 = new Piece(null, grid.getDefaultPieceBounds())
+
+        piece1.voxels = ["0,0,0", "1,1,1"]
+        piece2.voxels = ["0,0,0", "1,1,1"]
+        expect(piece1.equals(piece2)).toBeTruthy()
+
+        piece1.setVoxelAttribute("foo", "0,0,0", true)
+        expect(piece1.equals(piece2)).toBeFalsy()
+
+        piece2.setVoxelAttribute("foo", "0,0,0", false)
+        expect(piece1.equals(piece2)).toBeFalsy()
+
+        piece2.setVoxelAttribute("foo", "0,0,0", true)
+        expect(piece1.equals(piece2)).toBeTruthy()
+
+        piece1.setVoxelAttribute("bar", "0,0,0", true)
+        expect(piece1.equals(piece2)).toBeFalsy()
+
+        piece2.setVoxelAttribute("bar", "0,0,0", true)
+        expect(piece1.equals(piece2)).toBeTruthy()
+
+        // Attribute on non-existant voxel doesn't affect equality
+        piece2.setVoxelAttribute("baz", "8,8,8", true)
+        expect(piece1.equals(piece2)).toBeTruthy()
+    })
+
+    test("transform", () => {
+        const piece = new Piece(null, grid.getDefaultPieceBounds())
+        piece.voxels = ["0,0,0", "1,1,1"]
+
+        const translate = grid.getTranslation("0,0,0", "1,0,0")
+        piece.transform(translate)
+        expect(piece.voxels).toEqual(["1,0,0", "2,1,1"])
+    })
+
+    test("transform with voxelAttributes", () => {
+        const piece = new Piece(null, grid.getDefaultPieceBounds())
+        piece.voxels = ["0,0,0", "1,1,1"]
+        piece.setVoxelAttribute("foo", "0,0,0", true)
+
+        const translate = grid.getTranslation("0,0,0", "1,0,0")
+        piece.transform(translate)
+        expect(piece.voxels).toEqual(["1,0,0", "2,1,1"])
+        expect(piece.voxelAttributes).toEqual({
+            "foo": {
+                "1,0,0": true
+            }
+        })
+
+        // Transform when voxel attribute on a voxel that's not a part of this
+        // piece
+        piece.setVoxelAttribute("foo", "9,9,9", true)
+        piece.transform(translate)
+        expect(piece.voxels).toEqual(["2,0,0", "3,1,1"])
+        expect(piece.voxelAttributes).toEqual({
+            "foo": {
+                "2,0,0": true
+            }
+        })
     })
 })
--- lib/Puzzle.ts
+++ lib/Puzzle.ts
@@ -129,6 +129,8 @@ export class Puzzle extends SerializableClass {
     }
 }
 
+type AttributeValue = boolean
+
 export class Piece extends SerializableClass {
     id: string | null
 
@@ -137,6 +139,12 @@ export class Piece extends SerializableClass {
     label: string
     color: string
 
+    voxelAttributes?: {
+        [attribute: string]: {
+            [voxel: Voxel]: AttributeValue
+        }
+    }
+
     constructor(id: string | null, bounds: Bounds, voxels: Voxel[]=[]) {
         super()
         this.id = id
@@ -144,6 +152,7 @@ export class Piece extends SerializableClass {
         this.voxels = voxels
         this.label = id || "unlabeled-piece"
         this.color = "#00ff00"
+        this.voxelAttributes = undefined
     }
 
     hasId(): this is PieceWithId {
@@ -160,14 +169,76 @@ export class Piece extends SerializableClass {
         // Use sets so duplicate voxels don't affect equality
         const thisVoxels = new Set(this.voxels)
         const otherVoxels = new Set(other.voxels)
-        return thisVoxels.size === otherVoxels.size && [...thisVoxels].every(
-            v => otherVoxels.has(v)
-        )
+        if(
+            thisVoxels.size !== otherVoxels.size ||
+            [...thisVoxels].some(v => !otherVoxels.has(v))
+        ) {
+            return false
+        }
+
+        const thisAttrs = this.voxelAttributes || {}
+        const otherAttrs = other.voxelAttributes || {}
+        const attrNames = new Set([
+            ...Object.keys(thisAttrs),
+            ...Object.keys(otherAttrs)
+        ])
+        for(const attr of attrNames.keys()) {
+            const thisVoxelValues = thisAttrs[attr] || {}
+            const otherVoxelValues = otherAttrs[attr] || {}
+            const voxels = new Set([
+                ...Object.keys(thisVoxelValues),
+                ...Object.keys(otherVoxelValues)
+            ])
+            for(const voxel of voxels.keys()) {
+                if(!thisVoxels.has(voxel)) {
+                    continue
+                }
+
+                const thisValue = thisVoxelValues[voxel]
+                const otherValue = otherVoxelValues[voxel]
+                if(thisValue !== otherValue) {
+                    return false
+                }
+            }
+        }
+
+        return true
     }
 
     transform(transform: Transform): this {
-        this.voxels = transform.mapVoxels(this.voxels)
+        const newVoxels = transform.mapVoxels(this.voxels)
+
+        // Transform attributes based map of old to new voxels
+        const newAttrs: {
+            [attribute: string]: {
+                [voxel: Voxel]: AttributeValue
+            }
+        } = {}
+        for(const [attrName, attrValues] of Object.entries(this.voxelAttributes || {})) {
+            newAttrs[attrName] = {}
+            for(const [voxel, value] of Object.entries(attrValues)) {
+                const voxelIdx = this.voxels.indexOf(voxel)
+                if(voxelIdx === -1) {
+                    continue
+                }
+                const newVoxel = newVoxels[voxelIdx]
+                newAttrs[attrName][newVoxel] = value
+            }
+        }
+        this.voxelAttributes = newAttrs
+
+        this.voxels = newVoxels
         return this
+    }
+
+    setVoxelAttribute(attribute: string, voxel: Voxel, value: AttributeValue) {
+        if(!this.voxelAttributes) {
+            this.voxelAttributes = {}
+        }
+        if(!this.voxelAttributes[attribute]) {
+            this.voxelAttributes[attribute] = {}
+        }
+        this.voxelAttributes[attribute][voxel] = value
     }
 }
 
