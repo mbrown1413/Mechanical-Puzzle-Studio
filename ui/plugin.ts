46ce2aa5ed1d76d61d7a212f22de1329c74e3c95
Michael Brown <michael@msbrown.net>
2026-04-06 23:05:56 -0400

    Plugin hot module replacement
    This allows you to edit plugins in plugin/ without having the vite dev
    server refresh the whole page on each change.

--- ui/plugin.ts
+++ ui/plugin.ts
@@ -6,6 +6,7 @@ export type PluginContext = {
 
 export type PuzzleStudioPlugin = {
     setup(ctx: PluginContext): void
+    cleanup?(ctx: PluginContext): void
 }
 
 const pluginModules = import.meta.glob(
@@ -16,32 +17,73 @@ const pluginModules = import.meta.glob(
         import: "default",
     }
 )
+const loadedPlugins = new Map<string, PuzzleStudioPlugin>()
 
 export function definePlugin(plugin: PuzzleStudioPlugin): PuzzleStudioPlugin {
     return plugin
 }
 
-export async function loadPlugins() {
-    for (const [path, pluginModule] of Object.entries(pluginModules)) {
-        const context: PluginContext = {
-            registerClass,
-        }
-
-        let plugin
-        try {
-            plugin = await pluginModule() as PuzzleStudioPlugin
-        } catch (e) {
-            console.error(`Failed to load plugin from ${path}:\n`, e)
-            continue
-        }
-
-        try {
-            plugin.setup(context)
-        } catch (e) {
-            console.error(`Failed to setup plugin from ${path}:\n`, e)
-            continue
-        }
-
-        console.log(`Loaded plugin from ${path}`)
+function getPluginContext(): PluginContext {
+    return {
+        registerClass,
     }
+}
+
+function unloadPlugin(path: string) {
+    const plugin = loadedPlugins.get(path)
+    if(!plugin) {
+        return
+    }
+
+    loadedPlugins.delete(path)
+    if(!plugin.cleanup) {
+        return
+    }
+
+    try {
+        plugin.cleanup(getPluginContext())
+    } catch(e) {
+        console.error(`Failed to cleanup plugin from ${path}:\n`, e)
+    }
+}
+
+async function loadPlugin(path: string) {
+    const pluginModule = pluginModules[path]
+    if(!pluginModule) {
+        return
+    }
+
+    unloadPlugin(path)
+
+    let plugin
+    try {
+        plugin = await pluginModule() as PuzzleStudioPlugin
+    } catch(e) {
+        console.error(`Failed to load plugin from ${path}:\n`, e)
+        return
+    }
+
+    try {
+        plugin.setup(getPluginContext())
+        loadedPlugins.set(path, plugin)
+    } catch(e) {
+        console.error(`Failed to setup plugin from ${path}:\n`, e)
+        return
+    }
+
+    console.log(`Loaded plugin from ${path}`)
+}
+
+export async function loadPlugins() {
+    for(const path of Object.keys(pluginModules)) {
+        await loadPlugin(path)
+    }
+}
+
+if(import.meta.hot) {
+    import.meta.hot.accept(async (newModule) => {
+        if(loadedPlugins.size > 0) {
+            await newModule?.loadPlugins()
+        }
+    })
 }
